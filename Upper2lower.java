d0620cac8a5b181678d8dbea072bafb5f89b8f12
MMoA16 <83120699+MMoA16@users.noreply.github.com>
2021-04-24 17:22:42 +0530

    Create Upper2lower.java

---
+++ Upper2lower.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static void main(String[] args){
+    char c;
+    int p;
+    Scanner sc = new Scanner(System.in);
+    c=sc.next().charAt(0);
+    p=(int)c;
+    System.out.println((char)(p|32));
+  }
+}
+    
