63c9753ad9ef83401d0fc8f61a2246e3f38c935f
MMoA16 <83120699+MMoA16@users.noreply.github.com>
2021-05-09 02:13:09 +0530

    Update pascal_1d.java

--- pascal_1d.java
+++ pascal_1d.java
@@ -1,1 +1,32 @@
+import java.io.*;
+import java.util.Scanner;
+class pas1 {
+    public static void main(String[] args){
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n=sc.nextInt();
+        int k;
+        k=sc.nextInt();
+        int i;
+        int j;
+        int c[];
+        c=new int[k+1];
+        for(i=0;i<k;i++){
+            c[i]=0;
+        }
+        c[0]=1;
+        for(i=1;i<=n;i++){
+            for(j=min(i,k);j>0;j--){
+                c[j]=c[j-1]+c[j];
+            }
+        }
+        System.out.println(c[k]);
+}
 
+    private static int min(int i, int k) {
+        if(i>k)
+            return k;
+        else
+            return i;
+    }
+}
